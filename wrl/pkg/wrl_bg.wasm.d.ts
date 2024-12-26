16437a09414860fcf213c2c2be2cc8c023f35e18
Shaunak Kishore <kshaunak@gmail.com>
2024-12-26 00:32:25 -0500

    Add WildsRL demo

---
+++ wrl/pkg/wrl_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function keydown(a: number, b: number): void;
+export function tick(): void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_exn_store(a: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_start(): void;
